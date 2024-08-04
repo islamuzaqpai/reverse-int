d4c9c673191b8986e323cd1f64a0baf260c32080
Islam Uzakpai <166946427+islamuzaqpai@users.noreply.github.com>
2024-08-04 18:38:50 +0500

    init: Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let nStr = n.toString();
+    let length = nStr.length - 1;
+    let result = '';
+    let i = 0;
+    while (i <= length) {
+        result += nStr[length - i]
+        i++;
+    }
+    return parseInt(result);
 }
